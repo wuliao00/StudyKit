a400244267cd825334893fff94e8837201aacc43
wuliao00 <m15976065663@hotmail.com>
2026-09-21 08:37:02 +0800

    fix(dict): rememberSaveable 在 runtime.saveable 包下

--- app/src/main/java/com/studykit/ui/study/DictStoreScreen.kt
+++ app/src/main/java/com/studykit/ui/study/DictStoreScreen.kt
@@ -25,7 +25,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
